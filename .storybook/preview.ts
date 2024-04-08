5706fa81755c0be45c1e7c9b17c42eccf0253dc6
Sergei Sulimov <v-ssulimov@microsoft.com>
2024-04-08 23:38:26 +0400

    + storubook's viewports for descktop and retina

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -12,6 +12,45 @@ const preview: Preview = {
             },
         },
         layout: 'fullscreen',
+        viewport: {
+            viewports: {
+                mobile1: {
+                    name: 'Mobile 1',
+                    styles: {
+                        height: '640px',
+                        width: '360px',
+                    },
+                },
+                mobile2: {
+                    name: 'Mobile 2',
+                    styles: {
+                        height: '800px',
+                        width: '480px',
+                    },
+                },
+                tablet: {
+                    name: 'Tablet',
+                    styles: {
+                        height: '1024px',
+                        width: '768px',
+                    },
+                },
+                desktop: {
+                    name: 'Desktop',
+                    styles: {
+                        height: '1080px',
+                        width: '1920px',
+                    },
+                },
+                retina4k: {
+                    name: 'Retina 4K',
+                    styles: {
+                        height: '4096px',
+                        width: '2304px',
+                    },
+                },
+            },
+        },
     },
 };
 

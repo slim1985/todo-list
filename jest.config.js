67f88346fb38b808d0d6eb4c822c6aa237b8bb99
Sergei Sulimov <v-ssulimov@microsoft.com>
2024-04-12 11:50:11 +0400

    +jest configuration and taskService test

---
+++ jest.config.js
@@ -0,0 +1,36 @@
+/** @type {import('jest').Config} */
+const config = {
+    // Indicates which provider should be used to instrument code for coverage
+    coverageProvider: 'v8',
+
+    // A list of reporter names that Jest uses when writing coverage reports
+    coverageReporters: ['text'],
+
+    // An array of directory names to be searched recursively up from the requiring module's location
+    moduleDirectories: ['node_modules'],
+
+    transform: { '\\.[tj]sx?$': 'ts-jest' },
+
+    // The test environment that will be used for testing
+    testEnvironment: 'jsdom',
+
+    // The glob patterns Jest uses to detect test files
+    testMatch: [
+        '**/__tests__/**/*.[jt]s?(x)',
+        '**/?(*.)+(spec|test).[tj]s?(x)',
+    ],
+
+    // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+    testPathIgnorePatterns: ['\\\\node_modules\\\\', '\\\\dist\\\\'],
+
+    collectCoverageFrom: [
+        'src/**/*.{js,jsx,ts,tsx}',
+        '!src/**/*.stories.{js,jsx,ts,tsx}',
+        '!src/types/*.ts',
+        '!src/utils/*.{js,ts}',
+        '!**/node_modules/**',
+        '!**/vendor/**',
+    ],
+};
+
+module.exports = config;
---
+++ src/__tests__/taskService.test.ts
@@ -0,0 +1,149 @@
+import { taskService } from '../services/taskService';
+import { Task, TaskStates } from '../types/task';
+
+jest.mock('../services/firebaseDb', () => {
+    return {
+        firebaseDb: {
+            getManyAsync: jest.fn(() => {
+                return [
+                    {
+                        id: '1',
+                        data: {
+                            title: 'Task 1',
+                            description: 'Description 1',
+                            status: TaskStates.NEW,
+                        },
+                    },
+                    {
+                        id: '2',
+                        data: {
+                            title: 'Task 2',
+                            description: 'Description 2',
+                            status: TaskStates.ACTIVE,
+                        },
+                    },
+                ];
+            }),
+            createOneAsync: jest.fn(() => {
+                return {
+                    id: '111',
+                    data: {
+                        title: 'Task 1',
+                        description: 'Description 1',
+                        status: '1',
+                    },
+                };
+            }),
+            updateOneAsync: jest.fn(() => {
+                return {
+                    id: '1',
+                    data: {
+                        title: 'Task 2',
+                        description: 'Description 2',
+                        status: TaskStates.ACTIVE,
+                    },
+                };
+            }),
+            deleteOneAsync: jest.fn((docId: string) => Promise.resolve(docId)),
+        },
+    };
+});
+
+jest.mock('../services/authService', () => {
+    return {
+        authService: {
+            currentUserId: '1',
+        },
+    };
+});
+
+jest.mock('../services/firebaseApp', () => {
+    return {
+        firebaseApp: {},
+    };
+});
+
+describe('taskService', () => {
+    it('should get tasks', async () => {
+        // Arrange
+        // Act
+        const tasks = await taskService.getTasks();
+
+        // Assert
+        expect(tasks).toEqual([
+            {
+                id: '1',
+                title: 'Task 1',
+                description: 'Description 1',
+                status: TaskStates.NEW,
+            },
+            {
+                id: '2',
+                title: 'Task 2',
+                description: 'Description 2',
+                status: TaskStates.ACTIVE,
+            },
+        ]);
+    });
+
+    it('should create a task', async () => {
+        // Arrange
+        const task: Task = {
+            id: '',
+            title: 'Task 1',
+            description: 'Description 1',
+            status: TaskStates.ACTIVE,
+        };
+
+        // Act
+        const createdTask = await taskService.createTask(task);
+
+        // Assert
+        expect(createdTask).toEqual({
+            id: '111',
+            title: 'Task 1',
+            description: 'Description 1',
+            status: TaskStates.ACTIVE,
+        });
+    });
+
+    it('should update a task', async () => {
+        // Arrange
+        const task: Task = {
+            id: '1',
+            title: 'Task 1',
+            description: 'Description 1',
+            status: TaskStates.ACTIVE,
+        };
+
+        // Act
+        const updatedTask = await taskService.updateTask(task);
+
+        // Assert
+        expect(updatedTask).toEqual({
+            id: '1',
+            title: 'Task 2',
+            description: 'Description 2',
+            status: TaskStates.ACTIVE,
+        });
+    });
+
+    it('should delete a task', async () => {
+        // Arrange
+        const taskId = '1';
+
+        // Act
+        const deletedTaskId = await taskService.deleteTask(taskId);
+
+        // Assert
+        expect(deletedTaskId).toEqual(taskId);
+    });
+
+    it('should clear tasks', () => {
+        // Act
+        const clearedTasks = taskService.clearTasks();
+
+        // Assert
+        expect(clearedTasks).toEqual([]);
+    });
+});

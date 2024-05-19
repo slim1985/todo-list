acbc9b58e6e5392e046fc8b6a1b68e380b51ccb4
Sergei Sulimov <s.v.sulimoff@gmail.com>
2024-05-19 12:07:09 +0400

    + fix

--- src/hooks/useTasks.ts
+++ src/hooks/useTasks.ts
@@ -49,7 +49,7 @@ export function useTasks(): [
     }, [taskList]);
 
     useEffect(() => {
-        dispatch(getTasksAsync(null));
+        dispatch(getTasksAsync());
     }, []);
 
     function hideTaskForm(): void {
